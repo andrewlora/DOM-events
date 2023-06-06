b8537625f1e5cc1a3b1a43421f9ab0a51d94178a
Andres Lora <jalc.andres.lora@gmail.com>
2023-06-06 09:55:52 -0700

    if clicked on the list item, it toggles the .done class on and off

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.addEventListener("click", toggleItem);
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function toggleItem(item) {
+  item.target.classList.toggle("done");
 }
 
 button.addEventListener("click", addListAfterClick);
